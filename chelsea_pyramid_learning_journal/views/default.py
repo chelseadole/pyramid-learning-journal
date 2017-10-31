c206951fd281f25233c3ecc395a17b39108675de
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-31 16:17:17 -0700

    changing file paths

--- chelsea_pyramid_learning_journal/views/default.py
+++ chelsea_pyramid_learning_journal/views/default.py
@@ -8,14 +8,14 @@ from pyramid.view import view_config
 HERE = os.path.dirname(__file__)
 
 
-def list_vew(request):
+def list_view(request):
     """Parse file path and pass it to response to serve home page."""
     path = os.path.join(HERE, '../templates/homepage.html')
     with io.open(path) as file:
         return Response(file.read())
 
 
-def detail_vew(request):
+def detail_view(request):
     """Parse file path and pass it to response to serve home page."""
     path = os.path.join(HERE, '../templates/detail-entry.html')
     with io.open(path) as file:
