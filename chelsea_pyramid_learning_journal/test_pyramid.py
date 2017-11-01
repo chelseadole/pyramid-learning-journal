430837fb87d148397d336d86e602da789a774bf0
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-31 17:40:13 -0700

    adding new tests, testing for HTML file contents with DummyRequest

--- chelsea_pyramid_learning_journal/test_pyramid.py
+++ chelsea_pyramid_learning_journal/test_pyramid.py
@@ -33,6 +33,30 @@ def test_list_view_response_body_includes_our_page(dummy_request):
     assert the_tag in response.ubody
 
 
+def test_detail_view_response_body_includes_our_page(dummy_request):
+    """Test html contents in list view are the correct ones."""
+    from chelsea_pyramid_learning_journal.views.default import detail_view
+    response = detail_view(dummy_request)
+    the_tag = '<h1>CF 401, Day 01</h1>'
+    assert the_tag in response.ubody
+
+
+def test_create_view_response_body_includes_our_page(dummy_request):
+    """Test html contents in list view are the correct ones."""
+    from chelsea_pyramid_learning_journal.views.default import create_view
+    response = create_view(dummy_request)
+    the_tag = '<h2>New Entry</h2>'
+    assert the_tag in response.ubody
+
+
+def test_update_view_response_body_includes_our_page(dummy_request):
+    """Test html contents in list view are the correct ones."""
+    from chelsea_pyramid_learning_journal.views.default import update_view
+    response = update_view(dummy_request)
+    the_tag = '<p>Title:</p>'
+    assert the_tag in response.ubody
+
+
 def test_detail_view_response_status_200_ok(dummy_request):
     """Test detail view response status."""
     from chelsea_pyramid_learning_journal.views.default import detail_view
