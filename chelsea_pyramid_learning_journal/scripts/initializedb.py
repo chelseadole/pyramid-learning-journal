76c475e6aabcb14094a32267c3ec2181be37ee7c
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-03 16:25:37 -0700

    editing initdb file, using for loop to grab articles from LJ_entries.py in data.

--- chelsea_pyramid_learning_journal/scripts/initializedb.py
+++ chelsea_pyramid_learning_journal/scripts/initializedb.py
@@ -1,3 +1,5 @@
+"""Initialize Database through SQLAlchemy."""
+
 import os
 import sys
 import transaction
@@ -5,20 +7,23 @@ import transaction
 from pyramid.paster import (
     get_appsettings,
     setup_logging,
-    )
+)
 
 from pyramid.scripts.common import parse_vars
+from chelsea_pyramid_learning_journal.data.LJ_entries import POST
 
 from ..models.meta import Base
 from ..models import (
     get_engine,
     get_session_factory,
     get_tm_session,
-    )
+)
+
 from ..models import MyModel
 
 
 def usage(argv):
+    """DB usage."""
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri> [var=value]\n'
           '(example: "%s development.ini")' % (cmd, cmd))
@@ -26,6 +31,7 @@ def usage(argv):
 
 
 def main(argv=sys.argv):
+    """Main DB initialization."""
     if len(argv) < 2:
         usage(argv)
     config_uri = argv[1]
@@ -41,5 +47,7 @@ def main(argv=sys.argv):
     with transaction.manager:
         dbsession = get_tm_session(session_factory, transaction.manager)
 
-        model = MyModel(name='one', value=1)
-        dbsession.add(model)
+        db_entries = []
+        for entry in POST:
+            db_entries.append(MyModel(id=entry[id], author=entry[author], creation_date=entry[creation_date], title=entry[title], body=entry[body]))
+        dbsession.add(db_entries)
