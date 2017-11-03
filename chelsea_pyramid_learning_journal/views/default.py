c8801a30eeef14cdfd22e9b045ea6d27f1b77f68
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-02 21:48:32 -0700

    getting code working, changing update_view and edit_view to align

--- chelsea_pyramid_learning_journal/views/default.py
+++ chelsea_pyramid_learning_journal/views/default.py
@@ -2,11 +2,6 @@
 from pyramid.view import view_config
 from datetime import datetime
 from pyramid.httpexceptions import HTTPNotFound
-# from pyramid.response import Response
-# import os
-# import io
-
-# HERE = os.path.dirname(__file__)
 
 FMT = "%m/%d/%Y"
 
@@ -43,15 +38,14 @@ def list_view(request):
 def detail_view(request):
     """Parse file path and pass it to response to serve home page."""
     post_id = int(request.matchdict['id'])
-    # if post_id not in POST:
-    print(post_id)
     for post in POST:
         if post['id'] == post_id:
             return {'ljpost': post,
-                    'title': post.title,
+                    'title': post['title'],
                     'image': 'post-bg.jpg'}
 
     raise HTTPNotFound
+
 
 @view_config(route_name='create_view',
              renderer='chelsea_pyramid_learning_journal:templates/new-entry.jinja2')
@@ -63,16 +57,16 @@ def create_view(request):
 
 
 @view_config(route_name='update_view',
-             renderer='chelsea_pyramid_learning_journal:templates/templates/edit-entry.jinja2')
+             renderer='chelsea_pyramid_learning_journal:templates/edit-entry.jinja2')
 def update_view(request):
     """Parse file path and pass it to response to serve home page."""
     # get post ID from detail_view page... import it?
     # do same filter fn from detail_view
     post_id = int(request.matchdict['id'])
-    if post_id not in POST:
-        return HTTPNotFound
     for post in POST:
         if post['id'] == post_id:
-            return {'ljposts': post,
-                    'title': 'Update Entry',
+            return {'ljpost': post['body'],
+                    'title': post['title'],
                     'image': 'post-bg.jpg'}
+
+    raise HTTPNotFound
