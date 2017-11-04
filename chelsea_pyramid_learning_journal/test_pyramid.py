74e4835bc4060fd5e4edfa1d10d89538dd551fac
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-03 21:40:58 -0700

    adding some tests for step3.

--- chelsea_pyramid_learning_journal/test_pyramid.py
+++ chelsea_pyramid_learning_journal/test_pyramid.py
@@ -1,108 +1,105 @@
 """Test files for Pyramid Learning Journal."""
 
-from __future__ import unicode_literals
-from pyramid import testing
-from pyramid.httpexceptions import HTTPNotFound
 import pytest
+from pyramid import testing
+from chelsea_pyramid_learning_journal.models.mymodel import Journal
+from chelsea_pyramid_learning_journal.models.meta import Base
+from pyramid.httpexceptions import HTTPNotFound
 
 
-@pytest.fixture
-def dummy_request():
-    """Create dummy request fixture."""
-    return testing.DummyRequest()
+@pytest.fixture(scope='session')
+def configuration(request):
+    """Set up a Configurator instance."""
+    config = testing.setUp(settings={
+        'sqlalchemy.url': 'postgres://localhost:5432/test_journal'
+    })
+    config.include("chelsea_pyramid_learning_journal.models")
+    config.include("chelsea_pyramid_learning_journal.routes")
+
+    def teardown():
+        testing.tearDown()
+
+    request.addfinalizer(teardown)
+    return config
 
 
-def test_list_view_response_title(dummy_request):
-    """Test list view response of title."""
-    from chelsea_pyramid_learning_journal.views.default import list_view
-    response = list_view(dummy_request)
-    assert response['title'] == 'Chelsea LJ'
+@pytest.fixture(scope='session')
+def db_session(configuration, request):
+    """Create a session for interacting with the test database."""
+    SessionFactory = configuration.registry["dbsession_factory"]
+    session = SessionFactory()
+    engine = session.bind
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        session.transaction.rollback()
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return session
 
 
-def test_list_view_response_is_a_dictionary(dummy_request):
-    """Test that response to list_view is a dictionary."""
+@pytest.fixture(scope='session')
+def dummy_request(db_session):
+    """Instantiate a fake HTTP Request with a database session."""
+    return testing.DummyRequest(dbsession=db_session)
+
+
+def test_list_view_returns_dictionary(dummy_request):
+    """Test that list_view returns a dict from DB."""
     from chelsea_pyramid_learning_journal.views.default import list_view
     response = list_view(dummy_request)
     assert isinstance(response, dict)
 
 
-def test_list_view_response_has_good_img(dummy_request):
-    """Test that response to list_view has the right image."""
-    from chelsea_pyramid_learning_journal.views.default import list_view
-    response = list_view(dummy_request)
-    assert response['image'] == 'home-bg.jpg'
-
-
-def test_list_view_response_has_a_post(dummy_request):
-    """Test that response to list_view has the right image."""
-    from chelsea_pyramid_learning_journal.views.default import list_view
-    from chelsea_pyramid_learning_journal.data.LJ_entries import POST
-    response = list_view(dummy_request)
-    assert response['ljposts'] == POST
-
-
-def test_detail_view_has_correct_keys(dummy_request):
-    """Test that response to detail_view has the correct keys."""
-    from chelsea_pyramid_learning_journal.views.default import detail_view
-    dummy_request.matchdict['id'] = 1
-    response = detail_view(dummy_request)
-    assert 'image' in response
-    assert 'ljpost' in response
-    assert 'image' in response
-
-
-def test_http_not_found(dummy_request):
-    """Test that response to detail_view has the correct keys."""
-    from chelsea_pyramid_learning_journal.views.default import detail_view
-    dummy_request.matchdict['id'] = 50
-    with pytest.raises(HTTPNotFound):
-        assert detail_view(dummy_request)
-
-
-def test_new_entry_has_correct_response(dummy_request):
-    """Test that response to create_view_has_correct routing."""
+def test_list_view_response_has_image(dummy_request):
+    """Test that response to list_view has image."""
     from chelsea_pyramid_learning_journal.views.default import create_view
-    dummy_request.matchdict['id'] = 4
     response = create_view(dummy_request)
     assert response['title'] == 'Create New Entry'
 
 
-def test_new_entry_works_with_a_specific_entry(dummy_request):
-    """Test that response to create_view has the correct value."""
-    from chelsea_pyramid_learning_journal.views.default import create_view
-    dummy_request.matchdict['id'] = 7
-    response = create_view(dummy_request)
-    assert response['image'] == 'new-entry.jpg'
+def test_detail_view_only_shows_one_and_is_added_to_db(dummy_request):
+    """Item can be added to DB, and show in detail_view individually."""
+    from chelsea_pyramid_learning_journal.views.default import detail_view
+    assert len(db_session.query(Journal).all()) == 0
+    new_journal = Journal(
+        author='Chelsea Dole',
+        creation_date='11/02/2017',
+        title='Day 400',
+        body='Harry Potter and the Goblet of Fire'
+    )
+    dummy_request.dbsession.add(new_journal)
+    dummy_request.dbsession.commit()
+    assert len(db_session.query(Journal).all()) == 1
 
 
-def test_update_entry_works_for_response_title(dummy_request):
-    """Test that response to update_view has the correct title."""
-    from chelsea_pyramid_learning_journal.views.default import update_view
-    dummy_request.matchdict['id'] = 10
-    response = update_view(dummy_request)
-    assert response['title'] == 'Edit Entry'
+def test_journal_exists_and_is_in_list(dummy_request):
+    """Test interaction with DB."""
+    from chelsea_pyramid_learning_journal.views.default import list_view
+    new_journal = Journal(
+        author='Chelsea Dole',
+        creation_date='11/03/2017',
+        title='Day 15',
+        body='Harry Potter and the Prisoner of Azkaban.'
+    )
+    dummy_request.dbsession.add(new_journal)
+    dummy_request.dbsession.commit()
+    response = list_view(dummy_request)
+    assert isinstance(response, dict)
 
 
-def test_update_entry_post_content_loads_correctly(dummy_request):
-    """Test that response to update_view has the correct post."""
-    from chelsea_pyramid_learning_journal.views.default import update_view
-    dummy_request.matchdict['id'] = 12
-    response = update_view(dummy_request)
-    assert "10/31/2017" in response['ljpost']['creation_date']
-
-
-def test_update_entry_raises_http_error(dummy_request):
-    """Test that response to update_view raises httperror."""
-    from chelsea_pyramid_learning_journal.views.default import update_view
-    dummy_request.matchdict['id'] = 5000
+def test_detail_view_non_existent_expense(dummy_request):
+    """View detail with HTTPNotFound response."""
+    from chelsea_pyramid_learning_journal.views.default import detail_view
+    new_expense = Journal(
+        author='Chelsea Dole',
+        creation_date='11/02/2017',
+        title='Day 400',
+        body='Harry Potter and the Chamber of Secrets'
+    )
+    dummy_request.dbsession.add(new_expense)
+    dummy_request.dbsession.commit()
+    dummy_request.matchdict['id'] = 2000
     with pytest.raises(HTTPNotFound):
-        assert update_view(dummy_request)
-
-
-def test_update_entry_error_type(dummy_request):
-    """Test that httperror is 404."""
-    from chelsea_pyramid_learning_journal.views.default import update_view
-    dummy_request.matchdict['id'] = 2345
-    with pytest.raises(HTTPNotFound):
-        request = update_view(dummy_request)
-        request.response.status = 404
+        detail_view(dummy_request)
