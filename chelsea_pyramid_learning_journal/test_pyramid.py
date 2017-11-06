fc151c7b7ed310059fb4bbc30fe6a233b5aa5e48
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-06 13:47:37 -0800

    changes for testing.

--- chelsea_pyramid_learning_journal/test_pyramid.py
+++ chelsea_pyramid_learning_journal/test_pyramid.py
@@ -117,3 +117,15 @@ def test_create_view_still_works(dummy_request):
     dummy_request.matchdict['id'] = 5
     response = create_view(new_entry)
     assert response['title'] == 'Create New Entry'
+
+
+def test_list_view_return_journal_instance_with_incomplete_info(dummy_request):
+    """Update view response has file content."""
+    from chelsea_pyramid_learning_journal.views.default import list_view
+    new_journal = Journal(
+        title='this is an incomplete entry',
+    )
+    dummy_request.dbsession.add(new_journal)
+    request = dummy_request
+    response = list_view(request)
+    assert 'creation_date' not in response
