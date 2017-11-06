2788285bd6ca60f91df15feb3c2a83a9d7572be4
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-06 14:11:01 -0800

    moving tox.ini to mirror step_3 changes.

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ requires = [
     'zope.sqlalchemy',
     'waitress',
     'psycopg2',
-    'tox'
 ]
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
     'pytest',
     'pytest-cov',
+    'tox'
 ]
 
 setup(
