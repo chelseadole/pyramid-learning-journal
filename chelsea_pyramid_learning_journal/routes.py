9113fbd870ce7fc345b69da0c324031ae428ddcb
CHELSEA DOLE <chelseadole@gmail.com>
2017-11-07 17:30:05 -0800

    adding new routes in routes.py.

--- chelsea_pyramid_learning_journal/routes.py
+++ chelsea_pyramid_learning_journal/routes.py
@@ -8,3 +8,6 @@ def includeme(config):
     config.add_route('detail_view', '/journal/{id:\d+}')
     config.add_route('create_view', '/journal/new-entry')
     config.add_route('update_view', '/journal/{id:\d+}/edit-entry')
+    config.add_route('login', '/login')
+    config.add_route('logout', '/logout')
+    config.add_route('delete', '/delete')
